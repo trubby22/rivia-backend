c404bf2a6285430f15913b9a35103dd694be7fb6
barci2 <bcieslar2001@gmail.com>
2022-06-08 12:29:18 +0100

    More advanced DB fetching functions

--- src/main/kotlin/me/rivia/api/database/Utils.kt
+++ src/main/kotlin/me/rivia/api/database/Utils.kt
@@ -1,5 +1,7 @@
 package me.rivia.api.database
 
+import me.rivia.api.handlers.Meeting
+import me.rivia.api.handlers.Participant
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
@@ -15,9 +17,9 @@ val dbClient : DynamoDbClient = DynamoDbClient.builder()
 inline fun getEntry(
     tableName: String,
     primaryKeyName: String,
-    primaryKey: String
+    primaryKeyValue: String
 ): Map<String, AttributeValue>? {
-    val key = mapOf(primaryKeyName to AttributeValue.fromS(primaryKey))
+    val key = mapOf(primaryKeyName to AttributeValue.fromS(primaryKeyValue))
     val request = GetItemRequest.builder().tableName(tableName).key(key).build()
     return dbClient.getItem(request).item()
 }
@@ -35,11 +37,11 @@ inline fun fetchSingleBatch(
 inline fun getEntries(
     tableName: String,
     primaryKeyName: String,
-    primaryKeys: Iterable<String>
+    primaryKeyValues: Iterable<String>
 ): List<Map<String, AttributeValue>> {
     val primaryKeyMaps = LinkedList<Map<String, AttributeValue>>();
     val responses = LinkedList<Map<String, AttributeValue>>()
-    for (primaryKey in primaryKeys) {
+    for (primaryKey in primaryKeyValues) {
         primaryKeyMaps.add(mapOf(primaryKeyName to AttributeValue.fromS(primaryKey)))
         if (primaryKeyMaps.size == SINGLE_BATCH_LIMIT) {
             responses.addAll(fetchSingleBatch(tableName, primaryKeyMaps))
@@ -48,3 +50,19 @@ inline fun getEntries(
     responses.addAll(fetchSingleBatch(tableName, primaryKeyMaps))
     return responses
 }
+
+class FieldError(tableName: String, field: String) : Error("'$field' field of the '$tableName' table not present")
+
+inline fun getMeeting(meetingEntry : Map<String, AttributeValue>) = Meeting(
+        meetingEntry["title"]?.s() ?: throw FieldError("Meeting", "title"),
+        meetingEntry["startTime"]?.n()?.toInt() ?: throw FieldError("Meeting", "startTime"),
+        meetingEntry["endTime"]?.n()?.toInt() ?: throw FieldError("Meeting", "endTime"),
+    )
+
+inline fun getParticipant(participantEntry : Map<String, AttributeValue>) = Participant(
+    participant_id = participantEntry.get("UserID")?.s() ?: throw FieldError("User", "UserID"),
+    name = participantEntry["name"]?.s() ?: throw FieldError("User", "name"),
+    surname = participantEntry["surname"]?.s() ?: throw FieldError("User", "surname"),
+    email = participantEntry["email"]?.s() ?: throw FieldError("User", "email")
+)
+
