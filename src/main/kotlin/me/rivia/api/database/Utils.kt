2bf9db96e133093ee9d591168fce4a78cccb8500
barci2 <bcieslar2001@gmail.com>
2022-06-08 11:30:39 +0100

    Primitive DB fetching functions

--- src/main/kotlin/me/rivia/api/database/Utils.kt
+++ src/main/kotlin/me/rivia/api/database/Utils.kt
@@ -1,41 +1,50 @@
 package me.rivia.api.database
 
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
-import software.amazon.awssdk.enhanced.dynamodb.Key
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema
-import software.amazon.awssdk.enhanced.dynamodb.model.BatchGetItemEnhancedRequest
-import software.amazon.awssdk.enhanced.dynamodb.model.ReadBatch
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import software.amazon.awssdk.services.dynamodb.model.BatchGetItemRequest
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest
+import software.amazon.awssdk.services.dynamodb.model.KeysAndAttributes
+import java.util.LinkedList
 
-private val simpleClient = DynamoDbClient.builder()
+val dbClient : DynamoDbClient = DynamoDbClient.builder()
     .region(Region.EU_WEST_2)
     .build()
 
-val enhancedClient = DynamoDbEnhancedClient.builder().dynamoDbClient(simpleClient).build()
-
-inline fun <reified TableEntryType> getTable(tableName: String) =
-    enhancedClient.table(tableName, TableSchema.fromBean(TableEntryType::class.java))
-
-inline fun <reified TableEntryType> getEntry(
+inline fun getEntry(
     tableName: String,
+    primaryKeyName: String,
     primaryKey: String
-): TableEntryType? {
-    val table = getTable<TableEntryType>(tableName)
-    val key = Key.builder().partitionValue(primaryKey).build()
-    return table.getItem(key)
+): Map<String, AttributeValue>? {
+    val key = mapOf(primaryKeyName to AttributeValue.fromS(primaryKey))
+    val request = GetItemRequest.builder().tableName(tableName).key(key).build()
+    return dbClient.getItem(request).item()
 }
 
-inline fun <reified TableEntryType> getEntries(
+const val SINGLE_BATCH_LIMIT = 100
+inline fun fetchSingleBatch(
     tableName: String,
+    primaryKeyMaps: Collection<Map<String, AttributeValue>>,
+): List<Map<String, AttributeValue>> {
+    val keysAndAttributes = KeysAndAttributes.builder().keys(primaryKeyMaps).build()
+    val request = BatchGetItemRequest.builder().requestItems(mapOf(tableName to keysAndAttributes)).build()
+    return dbClient.batchGetItem(request).responses()?.get(tableName) ?: listOf()
+}
+
+inline fun getEntries(
+    tableName: String,
+    primaryKeyName: String,
     primaryKeys: Iterable<String>
-): List<TableEntryType> {
-    val table = getTable<TableEntryType>(tableName)
-    var batchGetItemBuilder = BatchGetItemEnhancedRequest.builder()
+): List<Map<String, AttributeValue>> {
+    val primaryKeyMaps = LinkedList<Map<String, AttributeValue>>();
+    val responses = LinkedList<Map<String, AttributeValue>>()
     for (primaryKey in primaryKeys) {
-        val key = Key.builder().partitionValue(primaryKey).build()
-        val readBatch = ReadBatch.builder(TableEntryType::class.java).mappedTableResource(table).addGetItem(key).build()
-        batchGetItemBuilder = batchGetItemBuilder.addReadBatch(readBatch)
+        primaryKeyMaps.add(mapOf(primaryKeyName to AttributeValue.fromS(primaryKey)))
+        if (primaryKeyMaps.size == SINGLE_BATCH_LIMIT) {
+            responses.addAll(fetchSingleBatch(tableName, primaryKeyMaps))
+        }
     }
-    return enhancedClient.batchGetItem(batchGetItemBuilder.build())
+    responses.addAll(fetchSingleBatch(tableName, primaryKeyMaps))
+    return responses
 }
--- src/main/kotlin/me/rivia/api/handlers/GetReview.kt
+++ src/main/kotlin/me/rivia/api/handlers/GetReview.kt
@@ -1,18 +1,12 @@
 package me.rivia.api.handlers
 
 import com.amazonaws.services.lambda.runtime.Context
-import main.MeetingDB
-import me.rivia.api.database.enhancedClient
 import me.rivia.api.database.getEntry
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable
-import software.amazon.awssdk.enhanced.dynamodb.Key
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema
-import software.amazon.awssdk.enhanced.dynamodb.model.GetItemEnhancedRequest
 
 class GetReview {
     // get data to display the review
     companion object {
-        data class ApiContext(val meeting_id: Uid?, val cookie: Int?) {
+        class ApiContext(var meeting_id: Uid?, var cookie: String?) {
             constructor() : this(null, null)
         }
 
@@ -26,45 +20,8 @@ class GetReview {
     }
 
     fun handle(input: ApiContext?, context: Context?): HttpResponse? {
-        // allows us to map the class with the fields stored in the table
-        val meetingTable: DynamoDbTable<MeetingDB> =
-            enhancedClient.table("Meeting", TableSchema.fromBean(MeetingDB::class.java))
-        val meetingEntry = getEntry<MeetingDB>("Meeting", input?.meeting_id ?: return null)
-
-        val participantTable: DynamoDbTable<Participant> =
-            enhancedClient.table("Participant", TableSchema.fromBean(Participant::class.java))
-
-
-        // get participant list info by going through users and finding their corresponding info in table USER
-
-        val participantList: MutableList<Participant> = mutableListOf()
-        // response type is 0 for the moment
-        for (user_id in result!!.participants) {
-            val userIDKey: Key = Key.builder().partitionValue(user_id).build()
-            val participantResult: Participant? =
-                participantTable.getItem { r: GetItemEnhancedRequest.Builder ->
-                    r.key(
-                        userIDKey
-                    )
-                }
-            if (participantResult != null) {
-                participantList.add(participantResult)
-            } else {
-                println("UserID associated with user in meeting not found")
-            }
-        }
-        // get question list (don't have seperate table at the moment)
-        val qList: MutableList<PresetQuestion> = mutableListOf()
-        var i = 0;
-        // asssigns 1,2,3,4... to list of questions
-        for (preset_q_text in result!!.preset_qs) {
-            qList.add(PresetQuestion(i.toString(), preset_q_text))
-            i++
-        }
-        return HttpResponse(
-            Meeting(result!!.title, result!!.startTime, result!!.endTime),
-            participantList.toTypedArray(),
-            qList.toTypedArray()
-        )
+        val meetingEntry = getEntry("Meeting", "MeetingID",  input?.meeting_id ?: throw Error("Meeting id not present")) ?: return null
+//        return meetingEntry.toString()
+        throw Error("Succeeded")
     }
 }
