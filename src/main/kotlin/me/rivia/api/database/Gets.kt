50dafc83f9e7fab76f59319c98fc53fd8d607115
Piotr Błaszyk <piopio555888@gmail.com>
2022-06-09 14:09:15 +0100

    [psb120] (feature) Refactored get helper methods to use the enhanced client

--- src/main/kotlin/me/rivia/api/database/Gets.kt
+++ src/main/kotlin/me/rivia/api/database/Gets.kt
@@ -3,20 +3,24 @@ package me.rivia.api.database
 import me.rivia.api.handlers.Meeting
 import me.rivia.api.handlers.Participant
 import me.rivia.api.handlers.PresetQuestion
+import me.rivia.api.handlers.Response
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable
+import software.amazon.awssdk.enhanced.dynamodb.Key
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema
+import software.amazon.awssdk.enhanced.dynamodb.model.ReadBatch
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import software.amazon.awssdk.services.dynamodb.model.BatchGetItemRequest
 import software.amazon.awssdk.services.dynamodb.model.GetItemRequest
 import software.amazon.awssdk.services.dynamodb.model.KeysAndAttributes
 import java.util.LinkedList
 
-fun getEntry(
+inline fun <reified EntryType> getEntry(
     tableName: String,
-    primaryKeyName: String,
-    primaryKeyValue: String
-): Map<String, AttributeValue>? {
-    val key = mapOf(primaryKeyName to AttributeValue.fromS(primaryKeyValue))
-    val request = GetItemRequest.builder().tableName(tableName).key(key).build()
-    return dbClient.getItem(request).item()
+    primaryKeyValue: String,
+): EntryType {
+    val table: DynamoDbTable<EntryType> = dbEnhancedClient
+        .table(tableName, TableSchema.fromClass(EntryType::class.java))
+    return table.getItem(Key.builder().partitionValue(primaryKeyValue).build())
 }
 
 //fun getEntryIndex(
@@ -27,13 +31,28 @@ fun getEntry(
 //
 //}
 
-private inline fun fetchSingleBatch(
+private inline fun <reified EntryType> fetchSingleBatch(
     tableName: String,
-    primaryKeyMaps: Collection<Map<String, AttributeValue>>,
-): List<Map<String, AttributeValue>> {
-    val keysAndAttributes = KeysAndAttributes.builder().keys(primaryKeyMaps).build()
-    val request = BatchGetItemRequest.builder().requestItems(mapOf(tableName to keysAndAttributes)).build()
-    return dbClient.batchGetItem(request).responses()?.get(tableName) ?: listOf()
+    primaryKeys: List<String>,
+): List<EntryType> {
+    val table: DynamoDbTable<EntryType> = dbEnhancedClient
+        .table(tableName, TableSchema.fromClass(EntryType::class.java))
+    return dbEnhancedClient.batchGetItem { r ->
+        r.addReadBatch(
+            ReadBatch
+                .builder(EntryType::class.java)
+                .mappedTableResource(table)
+                .apply {
+                    repeat(primaryKeys.size) { index ->
+                        this.addGetItem(
+                            Key.builder().partitionValue
+                                (primaryKeys[index]).build()
+                        )
+                    }
+                }
+                .build()
+        )
+    }.resultsForTable(table).toList()
 }
 
 fun getEntries(
@@ -41,11 +60,17 @@ fun getEntries(
     primaryKeyName: String,
     primaryKeyValues: Iterable<String>
 ): List<Map<String, AttributeValue>> {
-    val primaryKeyMaps = LinkedList<Map<String, AttributeValue>>();
+    val primaryKeyMaps = LinkedList<Map<String, AttributeValue>>()
     val responses = LinkedList<Map<String, AttributeValue>>()
     for (primaryKey in primaryKeyValues) {
-        primaryKeyMaps.add(mapOf(primaryKeyName to AttributeValue.fromS(primaryKey)))
-        if (primaryKeyMaps.size == SINGLE_BATCH_LIMIT) {
+        primaryKeyMaps.add(
+            mapOf(
+                primaryKeyName to AttributeValue.fromS(
+                    primaryKey
+                )
+            )
+        )
+        if (primaryKeyMaps.size > SINGLE_BATCH_LIMIT) {
             responses.addAll(fetchSingleBatch(tableName, primaryKeyMaps))
         }
     }
@@ -53,24 +78,43 @@ fun getEntries(
     return responses
 }
 
-class FieldError(tableName: String, field: String) : Error("'$field' field of the '$tableName' table not present")
-class PutError(tableName : String) : Error("Failed to put item into'$tableName")
+class FieldError(tableName: String, field: String) :
+    Error("'$field' field of the '$tableName' table not present")
 
-fun getMeeting(meetingEntry : Map<String, AttributeValue>) = Meeting(
-        meetingEntry["title"]?.s() ?: throw FieldError("Meeting", "title"),
-        meetingEntry["startTime"]?.n()?.toInt() ?: throw FieldError("Meeting", "startTime"),
-        meetingEntry["endTime"]?.n()?.toInt() ?: throw FieldError("Meeting", "endTime"),
-    )
+class PutError(tableName: String) :
+    Error("Failed to put item into'$tableName")
 
-fun getParticipant(participantEntry : Map<String, AttributeValue>) = Participant(
-    participantEntry.get("UserID")?.s() ?: throw FieldError("User", "UserID"),
-    participantEntry["name"]?.s() ?: throw FieldError("User", "name"),
-    participantEntry["surname"]?.s() ?: throw FieldError("User", "surname"),
-    participantEntry["email"]?.s() ?: throw FieldError("User", "email")
+fun getMeeting(meetingEntry: Map<String, AttributeValue>) = Meeting(
+    meetingEntry["title"]?.s() ?: throw FieldError("Meeting", "title"),
+    meetingEntry["startTime"]?.n()?.toInt() ?: throw FieldError(
+        "Meeting",
+        "startTime"
+    ),
+    meetingEntry["endTime"]?.n()?.toInt() ?: throw FieldError(
+        "Meeting",
+        "endTime"
+    ),
 )
 
+fun getParticipant(participantEntry: Map<String, AttributeValue>) =
+    Participant(
+        participantEntry.get("UserID")?.s() ?: throw FieldError(
+            "User",
+            "UserID"
+        ),
+        participantEntry["name"]?.s() ?: throw FieldError("User", "name"),
+        participantEntry["surname"]?.s() ?: throw FieldError(
+            "User",
+            "surname"
+        ),
+        participantEntry["email"]?.s() ?: throw FieldError("User", "email")
+    )
+
 fun getPresetQ(presetQEntry: Map<String, AttributeValue>) = PresetQuestion(
-    presetQEntry["PresetQID"]?.s() ?: throw FieldError("PresetQs", "PresetQID"),
+    presetQEntry["PresetQID"]?.s() ?: throw FieldError(
+        "PresetQs",
+        "PresetQID"
+    ),
     presetQEntry["text"]?.s() ?: throw FieldError("PresetQs", "text")
 )
 
