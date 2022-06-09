32ed5dc39f79bf3e94da91469b46c25b7e0f9e73
Piotr Błaszyk <piopio555888@gmail.com>
2022-06-09 17:26:40 +0100

    [psb120] (feature) Modified GetDashboard

--- src/main/kotlin/me/rivia/api/handlers/GetDashboard.kt
+++ src/main/kotlin/me/rivia/api/handlers/GetDashboard.kt
@@ -7,11 +7,12 @@ import me.rivia.api.database.getAllEntries
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema
+import me.rivia.api.database.Meeting as BackendMeeting
 
 class GetDashboard {
     class ApiContext(var cookie: Int? = null)
 
-    fun handle(input: ApiContext?, context: Context?): List<Meeting> {
-        return getAllEntries(Table.MEETING)
+    fun handle(input: ApiContext?, context: Context?): List<BackendMeeting> {
+        return getAllEntries<BackendMeeting>(Table.MEETING)
     }
 }
