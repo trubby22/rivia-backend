15bafe6b6aea8a7470c6bb2da0801785933c4ede
Piotr Błaszyk <piopio555888@gmail.com>
2022-06-09 14:22:46 +0100

    [psb120] (feature) Implemented getEntries

--- src/main/kotlin/me/rivia/api/database/Gets.kt
+++ src/main/kotlin/me/rivia/api/database/Gets.kt
@@ -1,26 +1,17 @@
 package me.rivia.api.database
 
-import me.rivia.api.handlers.Meeting
-import me.rivia.api.handlers.Participant
-import me.rivia.api.handlers.PresetQuestion
-import me.rivia.api.handlers.Response
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable
 import software.amazon.awssdk.enhanced.dynamodb.Key
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema
 import software.amazon.awssdk.enhanced.dynamodb.model.ReadBatch
-import software.amazon.awssdk.services.dynamodb.model.AttributeValue
-import software.amazon.awssdk.services.dynamodb.model.BatchGetItemRequest
-import software.amazon.awssdk.services.dynamodb.model.GetItemRequest
-import software.amazon.awssdk.services.dynamodb.model.KeysAndAttributes
-import java.util.LinkedList
 
 inline fun <reified EntryType> getEntry(
     tableName: String,
-    primaryKeyValue: String,
+    keyValue: String,
 ): EntryType {
     val table: DynamoDbTable<EntryType> = dbEnhancedClient
         .table(tableName, TableSchema.fromClass(EntryType::class.java))
-    return table.getItem(Key.builder().partitionValue(primaryKeyValue).build())
+    return table.getItem(Key.builder().partitionValue(keyValue).build())
 }
 
 //fun getEntryIndex(
@@ -31,9 +22,9 @@ inline fun <reified EntryType> getEntry(
 //
 //}
 
-private inline fun <reified EntryType> fetchSingleBatch(
+inline fun <reified EntryType> fetchSingleBatch(
     tableName: String,
-    primaryKeys: List<String>,
+    keys: List<String>,
 ): List<EntryType> {
     val table: DynamoDbTable<EntryType> = dbEnhancedClient
         .table(tableName, TableSchema.fromClass(EntryType::class.java))
@@ -43,10 +34,10 @@ private inline fun <reified EntryType> fetchSingleBatch(
                 .builder(EntryType::class.java)
                 .mappedTableResource(table)
                 .apply {
-                    repeat(primaryKeys.size) { index ->
+                    repeat(keys.size) { index ->
                         this.addGetItem(
                             Key.builder().partitionValue
-                                (primaryKeys[index]).build()
+                                (keys[index]).build()
                         )
                     }
                 }
@@ -55,66 +46,22 @@ private inline fun <reified EntryType> fetchSingleBatch(
     }.resultsForTable(table).toList()
 }
 
-fun getEntries(
+inline fun <reified EntryType> getEntries(
     tableName: String,
-    primaryKeyName: String,
-    primaryKeyValues: Iterable<String>
-): List<Map<String, AttributeValue>> {
-    val primaryKeyMaps = LinkedList<Map<String, AttributeValue>>()
-    val responses = LinkedList<Map<String, AttributeValue>>()
-    for (primaryKey in primaryKeyValues) {
-        primaryKeyMaps.add(
-            mapOf(
-                primaryKeyName to AttributeValue.fromS(
-                    primaryKey
-                )
-            )
-        )
-        if (primaryKeyMaps.size > SINGLE_BATCH_LIMIT) {
-            responses.addAll(fetchSingleBatch(tableName, primaryKeyMaps))
+    keys: Iterable<String>,
+): ArrayList<EntryType> {
+    val currentBatchKeys: MutableList<String> = mutableListOf()
+    val results: MutableList<EntryType> = mutableListOf()
+    for (key in keys) {
+        currentBatchKeys.add(key)
+        if (currentBatchKeys.size > SINGLE_BATCH_LIMIT) {
+            results.addAll(fetchSingleBatch(tableName, currentBatchKeys))
         }
     }
-    responses.addAll(fetchSingleBatch(tableName, primaryKeyMaps))
-    return responses
+    results.addAll(fetchSingleBatch(tableName, currentBatchKeys))
+    return ArrayList(results)
 }
-
-class FieldError(tableName: String, field: String) :
-    Error("'$field' field of the '$tableName' table not present")
 
 class PutError(tableName: String) :
     Error("Failed to put item into'$tableName")
-
-fun getMeeting(meetingEntry: Map<String, AttributeValue>) = Meeting(
-    meetingEntry["title"]?.s() ?: throw FieldError("Meeting", "title"),
-    meetingEntry["startTime"]?.n()?.toInt() ?: throw FieldError(
-        "Meeting",
-        "startTime"
-    ),
-    meetingEntry["endTime"]?.n()?.toInt() ?: throw FieldError(
-        "Meeting",
-        "endTime"
-    ),
-)
-
-fun getParticipant(participantEntry: Map<String, AttributeValue>) =
-    Participant(
-        participantEntry.get("UserID")?.s() ?: throw FieldError(
-            "User",
-            "UserID"
-        ),
-        participantEntry["name"]?.s() ?: throw FieldError("User", "name"),
-        participantEntry["surname"]?.s() ?: throw FieldError(
-            "User",
-            "surname"
-        ),
-        participantEntry["email"]?.s() ?: throw FieldError("User", "email")
-    )
-
-fun getPresetQ(presetQEntry: Map<String, AttributeValue>) = PresetQuestion(
-    presetQEntry["PresetQID"]?.s() ?: throw FieldError(
-        "PresetQs",
-        "PresetQID"
-    ),
-    presetQEntry["text"]?.s() ?: throw FieldError("PresetQs", "text")
-)
 
