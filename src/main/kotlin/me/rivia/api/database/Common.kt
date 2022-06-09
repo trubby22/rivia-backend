f60c0f49286912b0618a97c1083b1fdbde6a3fd6
barci2 <bcieslar2001@gmail.com>
2022-06-09 13:16:09 +0100

    added back the enhanced client variable
    Signed-off-by: barci2 <bcieslar2001@gmail.com>

--- src/main/kotlin/me/rivia/api/database/Common.kt
+++ src/main/kotlin/me/rivia/api/database/Common.kt
@@ -1,10 +1,33 @@
 package me.rivia.api.database
 
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 
-internal val dbClient : DynamoDbClient = DynamoDbClient.builder()
+val dbClient: DynamoDbClient = DynamoDbClient.builder()
     .region(Region.EU_WEST_2)
     .build()
 
-private const val SINGLE_BATCH_LIMIT = 100
+val dbEnhancedClient: DynamoDbEnhancedClient =
+    DynamoDbEnhancedClient.builder().dynamoDbClient(dbClient).build()
+
+interface DbEntry {
+    fun primaryKeyName(): String
+}
+
+const val SINGLE_BATCH_LIMIT = 100
+
+@DynamoDbBean
+data class Meeting(
+    @get:DynamoDbPartitionKey
+    var meetingId: String? = null,
+    var title: String? = null,
+    var organisation: String? = null,
+    var participants: Set<String>? = null,
+    var reviews: Set<String>? = null,
+    var startTime: Int? = null,
+    var endTime: Int? = null,
+)
