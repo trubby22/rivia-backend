4a18ac8922ab0e8b423d484af7e0409f387cabd1
Piotr Błaszyk <piopio555888@gmail.com>
2022-06-21 21:44:06 +0100

    [psb120] (refactor) Extracted a variable [psb120] (refactor) Refactored MS integration class

--- src/main/kotlin/me/rivia/api/teams/MicrosoftGraphClient.kt
+++ src/main/kotlin/me/rivia/api/teams/MicrosoftGraphClient.kt
@@ -2,34 +2,37 @@ package me.rivia.api.teams
 
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
-import me.rivia.api.database.*
+import me.rivia.api.database.Database
+import me.rivia.api.database.Table
 import me.rivia.api.database.entry.Tenant
+import me.rivia.api.database.getEntry
+import me.rivia.api.database.updateEntry
 import software.amazon.awssdk.http.HttpExecuteRequest
+import software.amazon.awssdk.http.HttpExecuteResponse
 import software.amazon.awssdk.http.SdkHttpMethod
 import software.amazon.awssdk.http.SdkHttpRequest
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient
+import java.io.IOException
 import java.net.URI
 import java.net.URLEncoder
-
-private data class TokenResponse(
-    @SerializedName("access_token") val accessToken: String,
-    @SerializedName("token_type") val tokenType: String,
-    @SerializedName("expires_in") val expiresIn: Int,
-    @SerializedName("scope") val scope: String,
-    @SerializedName("refresh_token") val refreshToken: String
-)
 
 enum class TokenType {
     APPLICATION, USER,
 }
 
-fun Any.utf8(): String = URLEncoder.encode(this.toString(), "UTF-8")
-
 class MicrosoftGraphClient(
     private val database: Database, private val tokenType: TokenType
 ) : TeamsClient {
-    companion object {
+    private val client = UrlConnectionHttpClient.builder().build()
 
+    companion object {
+        private data class TokenResponse(
+            @SerializedName("access_token") val accessToken: String?,
+            @SerializedName("token_type") val tokenType: String?,
+            @SerializedName("expires_in") val expiresIn: Int?,
+            @SerializedName("scope") val scope: String?,
+            @SerializedName("refresh_token") val refreshToken: String?,
+        )
     }
 
     private fun getAccessToken(tenantId: String): String {
@@ -52,46 +55,56 @@ class MicrosoftGraphClient(
         val clientId = "9661a5c4-6c18-49b8-aad6-e3a4722c2515"
         val scope =
             if (tokenType == TokenType.APPLICATION) "ChatMessage.Read.All" else "ChannelMessage.Send"
-        val redirectUri = "https://vbc48le64j.execute-api.eu-west-2.amazonaws.com/production/token"
+        val redirectUri =
+            "https://vbc48le64j.execute-api.eu-west-2.amazonaws.com/production"
         val clientSecret = "cap8Q~ESKP.5rpds2UeVfKw39.SB55YSmSwVmag8"
 
-        val client = UrlConnectionHttpClient.builder().build()
-
+        val sdkHttpRequest = SdkHttpRequest.builder().uri(
+            URI.create(
+                "https://login.microsoftonline.com/common/oauth2/v2.0/token"
+            )
+        ).putHeader("Content-Type", "application/x-www-form-urlencoded")
+            .method(SdkHttpMethod.POST)
+            .appendRawQueryParameter("client_id", clientId)
+            .appendRawQueryParameter("scope", scope).appendRawQueryParameter(
+                "refresh_token", getRefreshToken(tenantId)!!
+            ).appendRawQueryParameter("redirect_uri", redirectUri)
+            .appendRawQueryParameter("grant_type", "refresh_token")
+            .appendRawQueryParameter("client_secret", clientSecret).build()
         val request = client.prepareRequest(
             HttpExecuteRequest.builder().request(
-                SdkHttpRequest.builder().uri(
-                    URI.create(
-                        "https://login.microsoftonline.com/common/oauth2/v2.0/token"
-                    )
-                ).putHeader("Content-Type", "application/x-www-form-urlencoded")
-                    .method(SdkHttpMethod.POST).appendRawQueryParameter("client_id", clientId)
-                    .appendRawQueryParameter("scope", scope).appendRawQueryParameter(
-                        "refresh_token", getRefreshToken(tenantId)!!
-                    ).appendRawQueryParameter("redirect_uri", redirectUri)
-                    .appendRawQueryParameter("grant_type", "refresh_token")
-                    .appendRawQueryParameter("client_secret", clientSecret).build()
+                sdkHttpRequest
             ).build()
         )
 
-        val response = request.call()
+        val response: HttpExecuteResponse
 
-        client.close()
-
-        val inputStream = response.responseBody().get()
-        val jsonString = inputStream.readAllBytes().toString()
-
+        try {
+            response = request.call()
+        } catch (e: IOException) {
+            return null
+        } finally {
+            client.close()
+        }
+        if (!response.httpResponse().isSuccessful) {
+            return null
+        }
+        val jsonString = response.responseBody().get().readAllBytes().toString()
         val tokenResponse = Gson().fromJson(
             jsonString, TokenResponse::class.java
         )
-        val accessToken = tokenResponse.accessToken
-        val refreshToken = tokenResponse.refreshToken
+        setTokens(
+            tenantId,
+            tokenResponse.accessToken!!,
+            tokenResponse.refreshToken!!
+        )
 
-        setTokens(tenantId, accessToken, refreshToken)
-
-        return accessToken
+        return tokenResponse.accessToken
     }
 
-    override fun <T : Any> tokenOperation(tenantId: String, apiCall: (String) -> T?): T {
+    override fun <T : Any> tokenOperation(
+        tenantId: String, apiCall: (String) -> T?
+    ): T {
         var accessToken: String? = getAccessToken(tenantId)
         var result = apiCall(accessToken!!)
         while (result == null) {
@@ -120,4 +133,6 @@ class MicrosoftGraphClient(
         }
         return tenant != null
     }
+
+    private fun Any.utf8(): String = URLEncoder.encode(this.toString(), "UTF-8")
 }
