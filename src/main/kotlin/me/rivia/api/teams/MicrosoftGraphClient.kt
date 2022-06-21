923d598b2a09997bff1752f527855381ac16b804
barci2 <bcieslar2001@gmail.com>
2022-06-21 10:16:18 +0100

    minor formatting fixes
    Signed-off-by: barci2 <bcieslar2001@gmail.com>

--- src/main/kotlin/me/rivia/api/teams/MicrosoftGraphClient.kt
+++ src/main/kotlin/me/rivia/api/teams/MicrosoftGraphClient.kt
@@ -2,25 +2,28 @@ package me.rivia.api.teams
 
 import me.rivia.api.database.Database
 
-enum class RefreshTokenType {
+enum class TokenType {
     APPLICATION,
     USER,
 }
 
-class MicrosoftGraphClient(private val database: Database, private val refreshTokenType: RefreshTokenType) : TeamsClient {
+class MicrosoftGraphClient(private val database: Database, private val tokenType: TokenType) : TeamsClient {
     private fun getAccessToken(tenantId: String): String {
         TODO("Not yet implemented")
     }
 
-    private fun refreshAccessToken(tenantId: String): String {
+    private fun refreshAccessToken(tenantId: String): String? {
         TODO("Not yet implemented")
     }
 
     override fun <T : Any> tokenOperation(tenantId: String, apiCall: (String) -> T?): T {
-        var accessToken = getAccessToken(tenantId)
-        var result = apiCall(accessToken)
+        var accessToken: String? = getAccessToken(tenantId)
+        var result = apiCall(accessToken!!)
         while (result == null) {
-            accessToken = refreshAccessToken(tenantId)
+            do {
+                accessToken = refreshAccessToken(tenantId)
+            } while (accessToken == null)
+            result = apiCall(accessToken)
         }
         return result
     }
