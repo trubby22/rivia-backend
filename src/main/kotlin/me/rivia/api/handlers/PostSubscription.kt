633d952dfbda03989dbf73fe50d3be3dc0e70326
Piotr Błaszyk <piopio555888@gmail.com>
2022-06-21 23:54:27 +0100

    [psb120] (refactor) Refactored PostSubscription a bit

--- src/main/kotlin/me/rivia/api/handlers/PostSubscription.kt
+++ src/main/kotlin/me/rivia/api/handlers/PostSubscription.kt
@@ -5,7 +5,6 @@ import me.rivia.api.database.Database
 import me.rivia.api.teams.MicrosoftGraphClient
 import me.rivia.api.teams.TeamsClient
 import me.rivia.api.teams.TokenType
-import me.rivia.api.teams.utf8
 import me.rivia.api.userstore.UserStore
 import me.rivia.api.websocket.WebsocketClient
 import software.amazon.awssdk.http.HttpExecuteRequest
@@ -13,9 +12,14 @@ import software.amazon.awssdk.http.SdkHttpMethod
 import software.amazon.awssdk.http.SdkHttpRequest
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient
 import java.net.URI
+import java.net.URLEncoder
 import java.time.OffsetDateTime
 
 class PostSubscription : SubHandler {
+    companion object {
+        const val certificate = "placeholder"
+        const val certificateId = "placeholder"
+    }
     override fun handleRequest(
         url: List<String>,
         tenantId: String?,
@@ -27,52 +31,47 @@ class PostSubscription : SubHandler {
         applicationAccessToken: TeamsClient,
         websocket: WebsocketClient
     ): Response {
-
-        val params = mapOf(
-            "change_type" to "created",
-            "notificationUrl" to
-                    "https://vbc48le64j.execute-api.eu-west-2.amazonaws.com/production/graphEvent",
-            "resource" to "/teams/getAllMessages",
-            "expirationDateTime" to OffsetDateTime.now().plusHours(1),
-            "includeResourceData" to true,
-            "encryptionCertificate" to null,
-            "encryptionCertificateId" to null,
-        )
-        val urlParams =
-            params.map { (k, v) -> "${k.utf8()}=${v!!.utf8()}" }.joinToString("&")
-
         val client = UrlConnectionHttpClient.builder().build()
-
         val msClient = MicrosoftGraphClient(
-            database, TokenType
-                .APPLICATION
+            database, TokenType.APPLICATION
         )
-
         val request = { token: String ->
             client.prepareRequest(
-                HttpExecuteRequest.builder().request
-                    (
+                HttpExecuteRequest.builder().request(
                     SdkHttpRequest.builder().uri(
                         URI.create(
-                            "https://graph.microsoft.com/beta/subscriptions?${urlParams}"
+                            "https://graph.microsoft.com/beta/subscriptions"
                         )
-                    )
-                        .putHeader("Content-Type", "application/json")
-                        .putHeader(
+                    ).putHeader("Content-Type", "application/json").putHeader(
                             "Authorization", "Bearer $token"
-                        )
-                        .method(SdkHttpMethod.POST).build()
+                        ).method(SdkHttpMethod.POST)
+                        .appendRawQueryParameter("change_type", "created")
+                        .appendRawQueryParameter(
+                            "notificationUrl",
+                            "https://vbc48le64j.execute-api.eu-west-2.amazonaws.com/production/graphEvent"
+                        ).appendRawQueryParameter(
+                            "resource", "/teams/getAllMessages"
+                        ).appendRawQueryParameter(
+                            "expirationDateTime",
+                            OffsetDateTime.now().plusHours(1).utf8()
+                        ).appendRawQueryParameter(
+                            "includeResourceData", true.utf8()
+                        ).appendRawQueryParameter(
+                            "encryptionCertificate", certificate.utf8()
+                        ).appendRawQueryParameter(
+                            "encryptionCertificateId", certificateId.utf8()
+                        ).build()
                 ).build()
             )
         }
 
         val response = msClient.tokenOperation(tenantId!!, request).call()
-
-        client.close()
-
         val inputStream = response.responseBody().get()
         val jsonString = inputStream.readAllBytes().toString()
 
         return Response(jsonString)
     }
+
+    private fun Any?.utf8(): String =
+        URLEncoder.encode(this.toString(), "UTF-8")
 }
