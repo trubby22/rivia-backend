614b9d818e9a90fd496cbb7e214ae799146ea243
Joshua <josh.soyke-pinon20@imperial.ic.ac.uk>
2022-06-22 01:09:54 +0100

    refactored gson

--- src/main/kotlin/me/rivia/api/userstore/DatabaseUserStore.kt
+++ src/main/kotlin/me/rivia/api/userstore/DatabaseUserStore.kt
@@ -3,69 +3,90 @@ package me.rivia.api.userstore
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 import me.rivia.api.database.Database
 import me.rivia.api.database.Table
+import me.rivia.api.database.entry.User
 import me.rivia.api.database.getEntry
 import me.rivia.api.database.putEntry
 import me.rivia.api.teams.TeamsClient
-import java.net.URI
-import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient
 import software.amazon.awssdk.http.HttpExecuteRequest
 import software.amazon.awssdk.http.SdkHttpMethod
 import software.amazon.awssdk.http.SdkHttpRequest
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient
+import java.io.FileReader
+import java.io.IOException
+import java.net.URI
+
+
+data class UserResponse(
+    @SerializedName("@odata.context") val context : String,
+    @SerializedName("displayName") val displayName: String,
+    @SerializedName("surname") val surname: String,
+)
+
 
 class DatabaseUserStore(private val database: Database, private val applicationTeamsClient: TeamsClient) : UserStore {
-    override fun getUser(tenantId: String, userId: String): User? {
+    private val client = UrlConnectionHttpClient.builder().build()
+    private val gson : Gson = Gson();
+
+    override fun getUser(tenantId: String, userId: String): me.rivia.api.userstore.User? {
         // check if user is in database
-        val userEntry = database.getEntry<User>(Table.USERS, tenantId + userId)
-        if (userEntry == null) {
-            // create api lambda to use with access/refresh token in client
-            val apiCall = {accessToken : String ->
-                val client = UrlConnectionHttpClient.builder().build()
+        val userEntry : User? = database.getEntry<User>(Table.USERS, tenantId + userId)
 
-                val request = client.prepareRequest(
-                    HttpExecuteRequest.builder().request
-                        (
-                        SdkHttpRequest.builder().uri(
-                            URI.create(
-                                "https://graph.microsoft.com/v1.0/users/${userId}"
-                            )
-                        ).putHeader
-                            ("Content-Type", "application/json")
-                            .putHeader("Authorization", "Bearer $accessToken").method
+        if (userEntry != null) {
+            return me.rivia.api.userstore.User(
+                userEntry.tenantIdUserId!!, userEntry.name!!, userEntry.surname!!, userEntry.meetingIds!!
+            )
+        }
+        // make API call
+        val json = applicationTeamsClient.tokenOperation(tenantId) { accessToken: String ->
+            val response = client.prepareRequest(
+                HttpExecuteRequest.builder().request
+                    (
+                    SdkHttpRequest.builder().uri(
+                        URI.create(
+                            "https://graph.microsoft.com/v1.0/users/${userId}?select=displayName,surname"
+                        )
+                    ).putHeader
+                        ("Content-Type", "application/json")
+                        .putHeader("Authorization", "Bearer $accessToken").method
                             (SdkHttpMethod.GET).build()
-                    ).build()
-                )
+                ).build()
+            ).call()
 
-                val response = request.call()
-
+            if (!response.httpResponse().isSuccessful) {
+                System.err.println("HTTP failure")
                 client.close()
-
-                val inputStream = response.responseBody().get()
-                val json = inputStream.toString()
+                "error"
+            } else {
+                client.close()
+                val json = response.responseBody().get().readAllBytes().toString()
+                System.err.println(json)
                 json
             }
-
-            // make API call
-            val json = applicationTeamsClient.tokenOperation(tenantId, apiCall)
-            val jsonObject : JsonObject = JsonParser().parse(json).asJsonObject
-
-            // if id not found in microsoft database then return null
-            if (jsonObject.getAsJsonObject("error").asString != null) {
-                return null
-            }
-            val displayName : String = jsonObject.getAsJsonObject("displayName").asString;
-            val surname : String = jsonObject.getAsJsonObject("surname").asString;
-
-            // no meeting Ids when first adding user, will be updated by other code
-            val newUser = User(tenantId + userId, displayName, surname, listOf())
-            database.putEntry(Table.USERS, newUser);
-
-            return newUser
-        } else {
-            // otherwise already stored in "cache" database so just return
-            return userEntry;
         }
+        if (json == "error") {
+            System.err.println("HTTP error or client not found");
+            return null
+        }
+        val userResponse : UserResponse = gson.fromJson(json, UserResponse::class.java)
+
+
+        // no meeting Ids when first adding user, will be updated by other code
+        val newUser = User(tenantId + userId, userResponse.displayName, userResponse.surname, listOf())
+        database.putEntry(Table.USERS, newUser);
+        return me.rivia.api.userstore.User(
+            newUser.tenantIdUserId!!, newUser.name!!, newUser.surname!!, newUser.meetingIds!!
+        )
 
     }
 }
+fun main(args : Array<String>) {
+
+}
+
+
+
+
